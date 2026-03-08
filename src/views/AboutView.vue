8972cbe3e2bead56f4d7053a279c862a663392d7
Robson Mesquita Gomes <robson.mesquita56@gmail.com>
2026-03-08 13:53:28 -0300

    💄 feat(about): Update /about page text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Criado por <a class="link" target="_blank" href="https://robsongomes.me">Robson Gomes</a></h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+.about {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  min-height: 50vh;
+  justify-content: center;
+}
+
+a.link {
+  text-decoration: none;
+
+  color: var(--color-sky-400);
 }
 </style>
